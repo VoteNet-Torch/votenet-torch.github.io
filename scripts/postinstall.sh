75c1ef0895c9b0ef0844144ff4ed63842b2c0e63
SeanLi-OI <lixiang99410@126.com>
2021-02-10 11:31:54 +0800

    Initial commit

---
+++ scripts/postinstall.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cd "$(dirname "${0}")/.."
+APP_ROOT=$(pwd)
+
+function main() {
+	# install github1s extensions dependencies
+	for entry in "${APP_ROOT}/extensions"/*
+	do
+		if [ -d "$entry" ]
+		then
+			cd $entry
+			yarn --frozen-lockfile
+		fi
+	done
+
+	# clone vscode and install dependencies
+	cd ${APP_ROOT}
+	mkdir -p lib
+	cd lib
+	git clone --depth 1 -b 1.52.1 https://github.com/microsoft/vscode.git vscode
+	cd vscode
+	yarn --frozen-lockfile
+}
+
+main "$@"
